fa686e44ff74bda8d76e116031bfcce4cb3b1211
Letuchka11 <110563961+Letuchka11@users.noreply.github.com>
2022-08-17 09:35:25 -0800

    Update test.py

--- HW/files/test.py
+++ HW/files/test.py
@@ -2,6 +2,6 @@ from HW.file_executor.create import create_file
 from HW.file_executor.write import write_to_file
 from HW.file_executor.delete import delete_file
 
-create_file("mara", ".html")
-write_to_file("mara.py" , "'а дз именно через lib path и os делаются?? . на уроке не проходили ведь'")
-delete_file("marlen.html")
+create_file("mara", ".txt")
+write_to_file("mara.txt" , "'gu gu ga ga'")
+delete_file("marlen.txt")
